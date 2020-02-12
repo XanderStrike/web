36ad0cb7a1cc5b9a83341dc8cba4957ca68d17b9
Alex Standke <xanderstrike@gmail.com>
2020-02-12 14:06:42 -0800

    add server breakdown

--- astandke/build.sh
+++ astandke/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype index-unminified.html -o index.html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype server-unminified.html -o server.html
 
 docker build -t xanderstrike/web:astandke .
+docker push xanderstrike/web:astandke
